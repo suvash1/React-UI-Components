12c88afa900162b046a5a409a0930b232bbc570e
suvash1 <suvash1@gmail.com>
2019-06-02 10:55:22 -0400

    reached the mvp, added header, card and footer component and small components inside them

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className = "footer-icons">
+
+        </div>
+    );
+};
+
+export default Footer;
